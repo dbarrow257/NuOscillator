905d309d8a6288d69423a6ebb618e461f59dfdcf
Daniel Barrow <danbarrow257@gmail.com>
2023-06-06 16:58:00 +0100

    Maybe more than halfway through the documentation

--- OscProbCalcerBase.h
+++ OscProbCalcerBase.h
@@ -84,78 +84,246 @@ class OscProbCalcerBase {
    */
   void PrintWeights();
 
-  // Getters
+  /**
+   * @brief Return the number of oscillation parameters the specific implementation expects
+   * @return Return the number of oscillation parameters the specific implementation expects
+   */  
   int ReturnNOscParams() {return fNOscParams;}
+
+  /**
+   * @brief Return the oscillation parameters which were used for the last calculation
+   * @return Return the oscillation parameters which were used for the last calculation
+   */
   std::vector<FLOAT_T> ReturnOscParamsCurr() {return fOscParamsCurr;}
+
+  /**
+   * @brief Return the number of Energy points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   * @return Return the number of Energy points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   */
   int ReturnNEnergyPoints() {return fNEnergyPoints;}
+
+  /**
+   * @brief Return the number of CosineZ points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   * @return Return the number of CosineZ points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   */
   int ReturnNCosineZPoints() {return fNCosineZPoints;}
+
+  /**
+   * @brief Return the vector of Energy points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   * @return Return the vector of Energy points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   */
   std::vector<FLOAT_T> ReturnEnergyArray() {return fEnergyArray;}
+
+  /**
+   * @brief Return the vector of CosineZ points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   * @return Return the vector of CosineZ points which are being used by the specific instance of OscProbCalcerBase::OscProbCalcerBase()
+   */
   std::vector<FLOAT_T> ReturnCosineZArray() {return fCosineZArray;}
+
+  /**
+   * @brief Return the specific implementation name
+   * @return Return the specific implementation name
+   */
   std::string ReturnImplementationName() {return fImplementationName;}
+
+  /**
+   * @brief Return a boolean which describes whether the specific implementation cares about CosineZ
+   * @return Return a boolean which describes whether the specific implementation cares about CosineZ
+   */
   bool ReturnCosineZIgnored() {return fCosineZIgnored;}
+
+  /**
+   * @brief Return the number of oscillation probabilities that are being calculated
+   * @return Return the number of oscillation probabilities that are being calculated
+   */
   int ReturnNWeights() {return fNWeights;}
+
+  /**
+   * @brief Return the vector of oscillation probabilites which have been calculated
+   * @return Return the vector of oscillation probabilites which have been calculated
+   */
   std::vector<FLOAT_T> ReturnWeightArray() {return fWeightArray;}
 
   // ========================================================================================================================================================================
   // Public virtual functions which need calculater specific implementations
 
  protected:
+
+  /**
+   * @brief Default constructor
+   */
   OscProbCalcerBase();
 
   // ========================================================================================================================================================================
   // Protected functions which are calculation implementation agnostic  
 
-  // Some implementations don't care about the CosineZ values, so this is a method to set fCosineZArraySet to true in those cases
+  /**
+   * @brief Some implementations don't care about the CosineZ values, so this is a method to set #fCosineZArraySet to true in those cases
+   *
+   * If the specific implementation does not include atmospheric implementations, then #fCosineZArraySet should be set to true such that the Sanity check passes. Also
+   * sets #fCosineZIgnored to be true if it should be ignored.
+   *
+   * @param Ignore A boolean which declares whether the CosineZ dimension should be ignored.
+   */
   void IgnoreCosineZBinning(bool Ignore);
 
-  // Check whether the oscillation parameters have changed since their previous value
+  /**
+   * @brief Check whether the oscillation parameters have changed since their previous value
+   *
+   * We can save a fairly expensive calculation by checking whether the oscillation parameters have been updated since the last call to CalculateProbabilities() , for the
+   * price of a couple of if statements. Whilst in normal MCMC running, this will almost always return true, for sigma variations of systematics, this will almost always
+   * return false
+   *
+   * @param OscParamsToCheck The oscillation parameters which have been requested for the next calculation
+   * @return Boolean whether the OscParamsToCheck match those saved from the last calculation
+   */
   bool AreOscParamsChanged(std::vector<FLOAT_T> OscParamsToCheck);
 
-  // Save the oscillation parameters which have been requested
+  /**
+   * @brief Save the oscillation parameters which have been requested
+   * 
+   * Save the oscillation parameters which have been used in the probability calculation to #fOscParamsCurr
+   *
+   * @param OscParamsToCheck Parameter set to save
+   */
   void SetCurrOscParams(std::vector<FLOAT_T> OscParamsToCheck);
 
-  // Reset the saved oscillation parameters
+  /**
+   * @brief (Re-)Initialise the saved oscillation parameters in #fOscParamsCurr
+   */
   void ResetCurrOscParams();
 
-  // Return the index in fCosineZArray for a particular value of CosineZ. If it's not found, throws an error
+  /**
+   * @brief Return the index in #fCosineZArray for a particular value of CosineZ. If it's not found, throws an error
+   *
+   * Determine the index in #fCosineZArray for a particular value of CosineZ
+   *
+   * @param CosineZVal Value to search for
+   * @return Index in #fCosineZArray
+   */
   int ReturnCosineZIndexFromValue(FLOAT_T CosineZVal);
 
-  // Return the index in fCosineZArray for a particular value of CosineZ. If it's not found, throws an error
+  /**
+   * @brief Return the index in #fEnergyArray for a particular value of Energy. If it's not found, throws an error
+   *
+   * Determine the index in #fEnergyArray for a particular value of Energy
+   *
+   * @param EnergyVal Value to search for
+   * @return Index in #fEnergyArray
+   */
   int ReturnEnergyIndexFromValue(FLOAT_T EnergyVal);
 
-  // Calculate the index in the NuType/NuFlav mapping for a given value 
+  /**
+   * @brief Return the index in #fInitialFlavours for a particular neutrino flavour
+   *
+   * Neutrino flavour mapping is defined in #fInitialFlavours, so for a particular events flavour, the index in the mapping is returned to aide in #fWeightArray mapping
+   *
+   * @param InitFlav Initial neutrino flavour
+   * @return Index in #fInitialFlavours
+   */
   int ReturnInitialIndexFromFlavour(int InitFlav);
+
+  /**
+   * @brief Return the index in #fFinalFlavours for a particular neutrino flavour
+   *
+   * Neutrino flavour mapping is defined in #fFinalFlavours, so for a particular events flavour, the index in the mapping is returned to aide in #fWeightArray mapping
+   *
+   * @param FinalFlav Final neutrino flavour
+   * @return Index in #fFinalFlavours
+   */
   int ReturnFinalIndexFromFlavour(int FinalFlav);
+
+  /**
+   * @brief Return the index in #fNeutrinoTypes for a particular neutrino flavour (neutrino or antineutrino)
+   *
+   * Neutrino flavour mapping is defined in #fNeutrinoTypes, so for a particular events flavour, the index in the mapping is returned to aide in #fWeightArray mapping
+   *
+   * @param NuFlav Initial neutrino type (neutrino or antineutrino) 
+   * @return Index in #fNeutrinoTypes
+   */
   int ReturnNuTypeFromFlavour(int NuFlav);
 
-  // Initialise the mapping arrays to a particular size with dummy values
+  /**
+   * @brief Initialise the #fNeutrinoTypes mapping array to a particular size with dummy values
+   *
+   * @param Size Size of array to initialise
+   */
   void InitialiseNeutrinoTypesArray(int Size);
+  
+  /**
+   * @brief Initialise the #fInitialFlavours mapping array to a particular size with dummy values
+   *
+   * @param Size Size of array to initialise
+   */
   void InitialiseInitialFlavoursArray(int Size);
+  
+  /**
+   * @brief Initialise the #fFinalFlavours mapping array to a particular size with dummy values
+   *
+   * @param Size Size of array to initialise
+   */
   void InitialiseFinalFlavoursArray(int Size);
 
-  // Check that the NuType/NuFlav mapping is set correctly based on the inputs from the particular implementation
+  /**
+   * @brief Check that the NuType/NuFlav mapping is set correctly based on the inputs from the particular implementation
+   *
+   * Ensures that the mapping variables (#fNeutrinoTypes, #fInitialFlavours, #fFinalFlavours) are the expected size and filled with reasonable values
+   */
   void CheckNuFlavourMapping();
 
-  // Initialise the array in which the oscillation probabilities will be stored.
+  /**
+   * @brief Initialise the array in which the oscillation probabilities will be stored with dummy values
+   */
   void IntialiseWeightArray();
 
-  // Ensure that the oscillation probabilities are within [0.,1.] range
+  /**
+   * @brief Ensure that the oscillation probabilities are within [0.,1.] range, if not throw error
+   */
   void SanitiseProbabilities();
 
   // ========================================================================================================================================================================
   // Protected virtual functions which are calculation implementation agnostic
 
-  // Calculate some oscillation probabilities for a particular oscillation parameter set
+  /**
+   * @brief Calculate some oscillation probabilities for a particular oscillation parameter set
+   *
+   * Calculator engine specific implementation to calculate the oscillation probabilties for the given oscillation parameters. This function both calculates and stores
+   * the oscillation probabilities in #fWeightArray.
+   *
+   * @param OscParams The parameter set to calculate oscillation probabilities at
+   */
   virtual void CalculateProbabilities(std::vector<FLOAT_T> OscParams) = 0;
 
-  // Setup any implementation specific variables/functions
+  /**
+   * @brief Setup any implementation specific variables/functions
+   *
+   * Calculator engine specific implementation to setup any variables needed to calculate the oscillation probabilities
+   */
   virtual void SetupPropagator() = 0;
 
-  // Return the index in the weight array for a specific combination of neutrino oscillation channel, energy and cosine zenith
+  /**
+   * @brief Return the index in the weight array for a specific combination of neutrino oscillation channel, energy and cosine zenith
+   *
+   * Depending upon the specific implementation, there maybe different ways to calculate the oscillation probabilites (i.e. all oscillation channels at once or seperately).
+   * This function allows a mapping between the neutrino flavour to calculate for (along with Energy and CosineZ) to the index in #fWeightArray.
+   * 
+   * @param NuTypeIndex The index in #fNeutrinoTypes (neutrino/antinuetrino) to return the pointer for 
+   * @param InitNuIndex The index in #fInitialFlavours (electron/muon/tau) to return the pointer for 
+   * @param FinalNuIndex The index in #fFinalFlavours (electron/muon/tau) to return the pointer for 
+   * @param EnergyIndex The index in #fEnergyArray to return the pointer for 
+   * @param CosineZIndex The index in #fCosineZArray to return the pointer for 
+   *
+   * @return Index in #fWeightArray which corresponds to the given inputs
+   */
   virtual int ReturnWeightArrayIndex(int NuTypeIndex, int InitNuIndex, int FinalNuIndex, int EnergyIndex, int CosineZIndex=-1) = 0;
 
-  // Define the size of fWeightArray. This is implementation specific because some propagators calculate all
-  // oscillation channels together, whilst others calculate only a single oscillation channel. 
+  /**
+   * @brief Define the size of fWeightArray
+   *
+   * This is implementation specific because some propagators calculate all oscillation channels together, whilst others calculate only a single oscillation channel.
+   *
+   * @return Length that #fWeightArray should be initialised to
+   */
   virtual long DefineWeightArraySize() = 0;
 
   // ========================================================================================================================================================================
@@ -164,46 +332,118 @@ class OscProbCalcerBase {
   // Enums to define the mappings below. Each implementation is expected to define a mapping of which initial and neutrino flavours are considered, along with whether
   // neutrinos and antineutrinos are considered
 
+  /**
+   * @brief The number of neutrino types (neutrino and antineutrino)
+   */
   int fNNeutrinoTypes;
+
+  /**
+   * @brief The mapping of neutrino types to uniquely defined integers (i.e. index)
+   */
   std::vector<int> fNeutrinoTypes;
+
+  /**
+   * @brief The number of initial neutrino flavours (electron, muon, tau)
+   */
   int fNInitialFlavours;
+
+  /**
+   * @brief The mapping of initial neutrino flavour to uniquely defined integers (i.e. index)
+   */
   std::vector<int> fInitialFlavours;
+
+  /**
+   * @brief The number of final neutrino flavours (electron, muon, tau)
+   */
   int fNFinalFlavours;
+
+  /**
+   * @brief The mapping of final neutrino flavour to uniquely defined integers (i.e. index)
+   */
   std::vector<int> fFinalFlavours;
 
-  // Store energy and cosine points which will be used when calculating oscillation probabilities
+  /**
+   * @brief The number of Energy points which are being evaluated by the oscillation probability engine
+   */
   int fNEnergyPoints;
+
+  /**
+   * @brief The number of CosineZ points which are being evaluated by the oscillation probability engine
+   */
   int fNCosineZPoints;
+
+  /**
+   * @brief The vector of Energy values being evaluated by the oscillation probability engine
+   */
   std::vector<FLOAT_T> fEnergyArray;
+
+  /**
+   * @brief The vector of CosineZ values being evaluated by the oscillation probability engine
+   */
   std::vector<FLOAT_T> fCosineZArray;
 
-  // Place to store the oscillation probabilities
+  /**
+   * @brief The number of oscillation probabilities being calculated
+   */
   int fNWeights;
+  
+  /**
+   * @brief Vector which stores the oscillation probabilities
+   */
   std::vector<FLOAT_T> fWeightArray;
 
-  // Store the oscillation parameter set used to calculate the previous and current probabilities
-  // These are used to check whether the oscillation parameters have been updated from the previous CalculateProbabilities(OscParams) call.
-  // If they haven't don't update the already calculated probabilities
+  /**
+   * @brief Store the oscillation parameter set used to calculate the previous and current probabilities
+   */
   int fNOscParams;
+
+  /**
+   * @brief Vector which stores the saved oscillation probabilities
+   */
   std::vector<FLOAT_T> fOscParamsCurr;
 
-  // Set some verbosity for console output
+  /**
+   * @brief Define the verbosity of the console output
+   */
   int fVerbose;
   enum Verbosity{NONE,INFO};
 
-  // Define the implementation name - Could be used for recasting later (similar to TObject::InheritsFrom() etc.)
+  /**
+   * @brief Define the implementation name - Can be used for recasting later (similar to TObject::InheritsFrom() etc.)
+   */
   std::string fImplementationName;
 
-  // Flag to define whether the CosineZ binning has been ignored in the specific implementation
+  /**
+   * @brief Flag to define whether the CosineZ binning has been ignored in the specific implementation
+   */
   bool fCosineZIgnored;
 
  private:
   // ========================================================================================================================================================================
   // Sanity check variables
+  /**
+   * @brief Boolean declaring whether the Energy array has been set
+   */
   bool fEnergyArraySet;
+
+  /**
+   * @brief Boolean declaring whether the CosineZ array has been set
+   */
   bool fCosineZArraySet;
+  
+  /**
+   * @brief Boolean declaring whether the specific propagator SetupPropagator() function has been called
+   */
   bool fPropagatorSet;
+
+  /**
+   * @brief Boolean declaring whether #fWeightArray has been initialised correctly
+   */
   bool fWeightArrayInit;
+
+  /**
+   * @brief Boolean declaring whether #fNeutrinoTypes, #fInitialFlavours and #fFinalFlavours have been initialised correctly
+   */
   bool fNuMappingSet;
 };
 
