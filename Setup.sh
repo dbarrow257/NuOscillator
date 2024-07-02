1f8eca3ea5f447a3d4b703c292aa8fa2178a5eaf
Daniel Barrow <danbarrow257@gmail.com>
2024-07-02 08:28:15 -0500

    New Day with Ed and Kamil - Logic change in CMake - Attempt 13

--- Setup.sh
+++ Setup.sh
@@ -1,48 +1,49 @@
 source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
 spack load root@6.28.06
 spack load gcc@12.2.0
+spack load cmake
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/Prob3plusplus:${PWD}/ProbGPU:
 
 #Environment configs
-export UseGPU=0
-export UseMultithreading=1
-export OMP_NUM_THREADS=8
-export UseDoubles=1
+#export UseGPU=0
+#export UseMultithreading=1
+#export OMP_NUM_THREADS=8
+#export UseDoubles=1
 
-if [ ${UseGPU} == 1 ]; then
-    echo "Using GPU"
-else
-    echo "Not using GPU"
-fi
+#if [ ${UseGPU} == 1 ]; then
+#    echo "Using GPU"
+#else
+#    echo "Not using GPU"
+#fi
 
-if [ ${UseMultithreading} == 1 ]; then
-    echo "Using Multithreading with:" ${OMP_NUM_THREADS} "threads"
-else
-    echo "Not using Multithreading"
-fi
+#if [ ${UseMultithreading} == 1 ]; then
+#    echo "Using Multithreading with:" ${OMP_NUM_THREADS} "threads"
+#else
+#    echo "Not using Multithreading"
+#fi
 
 #Which calculators to compile
-export UseCUDAProb3=0
-export UseCUDAProb3Linear=1
-export UseProbGPULinear=0
-export UseProb3ppLinear=1
+#export UseCUDAProb3=0
+#export UseCUDAProb3Linear=1
+#export UseProbGPULinear=0
+#export UseProb3ppLinear=1
 
 #ProbGPU only supported when using GPU
-if [ ${UseGPU} == 0 ]; then
-    export UseProbGPULinear=0
-fi
+#if [ ${UseGPU} == 0 ]; then
+#   export UseProbGPULinear=0
+#fi
 
 #Set CUDA environment variables
-if [ ${UseGPU} == 1 ]; then
-    export CUDAPATH=/usr/local/cuda/
-    export PATH=${PATH}:${CUDAPATH}/bin/
-fi 
+#if [ ${UseGPU} == 1 ]; then
+#    export CUDAPATH=/usr/local/cuda/
+#    export PATH=${PATH}:${CUDAPATH}/bin/
+#fi 
 
-if [ ${UseCUDAProb3} == 1 ] && [ ${UseCUDAProb3Linear} == 1 ]; then
-    echo "error: CUDAProb3 and CUDAProb3Linear and not able to be built at the same time"
-fi
+#if [ ${UseCUDAProb3} == 1 ] && [ ${UseCUDAProb3Linear} == 1 ]; then
+#    echo "error: CUDAProb3 and CUDAProb3Linear and not able to be built at the same time"
+#fi
 
-if [ ${UseCUDAProb3Linear} == 1 ]; then
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/CUDAProb3Linear/build
-fi
+#if [ ${UseCUDAProb3Linear} == 1 ]; then
+#    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/CUDAProb3Linear/build
+#fi
