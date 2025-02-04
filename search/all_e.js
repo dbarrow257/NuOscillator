3070f60735985645ee50cf23d4fac56369600940
dbarrow257 <dbarrow257@users.noreply.github.com>
2025-02-04 19:46:41 +0000

    deploy: 1057681c46020ad7629f9a1c3e8adaba11e9a9ba

--- search/all_e.js
+++ search/all_e.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['pmns_5fstrtoint_0',['PMNS_StrToInt',['../classOscProbCalcerOscProb.html#a9a6b242e6819e19b2880cab05cc6faa9',1,'OscProbCalcerOscProb']]],
   ['pmnsmatrix_1',['PMNSMatrix',['../classOscProbCalcerOscProb.html#a62fca4b88373b2123e942914e5c4d623',1,'OscProbCalcerOscProb']]],
-  ['printknownoscillationchannels_2',['PrintKnownOscillationChannels',['../classOscProbCalcerBase.html#a0607bb55dd7abfb62666de7a3132b313',1,'OscProbCalcerBase']]],
-  ['printweights_3',['printweights',['../classOscillatorBase.html#a8694c5f8be7e667eaaf6e30901dae167',1,'OscillatorBase::PrintWeights()'],['../classOscProbCalcerBase.html#a7a1502b29dfe0ea8e0e48725a4520abc',1,'OscProbCalcerBase::PrintWeights()']]],
-  ['probability_4',['Probability',['../structNuOscillator_1_1OscillationProbability.html#ae654427199fa3e6ba115b6a9d7ce2f1a',1,'NuOscillator::OscillationProbability']]],
-  ['propagator_5',['propagator',['../classcudaprob3_1_1Propagator.html',1,'cudaprob3::Propagator&lt; T &gt;'],['../classOscProbCalcerCUDAProb3.html#ac1f8de8d9c1ccd3895555d1fad6b5bad',1,'OscProbCalcerCUDAProb3::propagator'],['../classOscProbCalcerCUDAProb3Linear.html#a15abf4d1581782f379780e01c30cc31d',1,'OscProbCalcerCUDAProb3Linear::propagator']]]
+  ['precisionlimit_2',['PrecisionLimit',['../classOscProbCalcerBase.html#ad8011dc225caeb64d40bf2cb7df3af1e',1,'OscProbCalcerBase']]],
+  ['printknownoscillationchannels_3',['PrintKnownOscillationChannels',['../classOscProbCalcerBase.html#a0607bb55dd7abfb62666de7a3132b313',1,'OscProbCalcerBase']]],
+  ['printweights_4',['printweights',['../classOscillatorBase.html#a8694c5f8be7e667eaaf6e30901dae167',1,'OscillatorBase::PrintWeights()'],['../classOscProbCalcerBase.html#a7a1502b29dfe0ea8e0e48725a4520abc',1,'OscProbCalcerBase::PrintWeights()']]],
+  ['probability_5',['Probability',['../structNuOscillator_1_1OscillationProbability.html#ae654427199fa3e6ba115b6a9d7ce2f1a',1,'NuOscillator::OscillationProbability']]],
+  ['propagator_6',['propagator',['../classcudaprob3_1_1Propagator.html',1,'cudaprob3::Propagator&lt; T &gt;'],['../classOscProbCalcerCUDAProb3.html#ac1f8de8d9c1ccd3895555d1fad6b5bad',1,'OscProbCalcerCUDAProb3::propagator'],['../classOscProbCalcerCUDAProb3Linear.html#a15abf4d1581782f379780e01c30cc31d',1,'OscProbCalcerCUDAProb3Linear::propagator']]]
 ];
--- search/variables_8.js
+++ search/variables_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['probability_0',['Probability',['../structNuOscillator_1_1OscillationProbability.html#ae654427199fa3e6ba115b6a9d7ce2f1a',1,'NuOscillator::OscillationProbability']]],
-  ['propagator_1',['propagator',['../classOscProbCalcerCUDAProb3.html#ac1f8de8d9c1ccd3895555d1fad6b5bad',1,'OscProbCalcerCUDAProb3::propagator'],['../classOscProbCalcerCUDAProb3Linear.html#a15abf4d1581782f379780e01c30cc31d',1,'OscProbCalcerCUDAProb3Linear::propagator']]]
+  ['precisionlimit_0',['PrecisionLimit',['../classOscProbCalcerBase.html#ad8011dc225caeb64d40bf2cb7df3af1e',1,'OscProbCalcerBase']]],
+  ['probability_1',['Probability',['../structNuOscillator_1_1OscillationProbability.html#ae654427199fa3e6ba115b6a9d7ce2f1a',1,'NuOscillator::OscillationProbability']]],
+  ['propagator_2',['propagator',['../classOscProbCalcerCUDAProb3.html#ac1f8de8d9c1ccd3895555d1fad6b5bad',1,'OscProbCalcerCUDAProb3::propagator'],['../classOscProbCalcerCUDAProb3Linear.html#a15abf4d1581782f379780e01c30cc31d',1,'OscProbCalcerCUDAProb3Linear::propagator']]]
 ];
