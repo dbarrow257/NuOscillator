aebc65837b7cd35554211b9890a6bf04197c576e
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2025-02-06 08:28:36 +0000

    test

--- Setup.sh
+++
@@ -1,6 +0,0 @@
-source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
-spack load root@6.28.06
-spack load gcc@12.2.0
-spack load cmake
-
-source /cvmfs/larsoft.opensciencegrid.org/spack-packages/opt/spack/linux-almalinux9-x86_64_v2/gcc-12.2.0/root-6.28.06-jhpj2jsdlwoxbvpnwmxvzkntrxcgw5of/bin/thisroot.sh
