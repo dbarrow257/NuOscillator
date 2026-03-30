c1cb69a7a09df83385fa9efb4502a940ad734fda
Daniel Barrow <daniel.barrow@physics.ox.ac.uk>
2026-03-30 09:42:43 +0100

    Add files

---
+++ OscProbCalcer/OscProbCalcer_NuFASTEarth.cpp
@@ -0,0 +1,81 @@
+#include "OscProbCalcer_NuFASTEarth.h"
+
+#include "NuFastEarth.h"
+#include "Matrix.h"
+
+OscProbCalcerNuFASTEarth::OscProbCalcerNuFASTEarth(YAML::Node Config_) : OscProbCalcerBase(Config_)
+{
+  //=======
+  //Grab information from the config
+
+  DetectorDepth = 2.0; // detector depth in km
+  
+  //=======
+  fNOscParams = kNOscParams;
+
+  fNNeutrinoTypes = 2;
+  InitialiseNeutrinoTypesArray(fNNeutrinoTypes);
+  fNeutrinoTypes[0] = Nu;
+  fNeutrinoTypes[1] = Nubar;
+  
+  // This implementation only considers atmopsheric propagation, thus no requirement to set cosineZ array
+  IgnoreCosineZBinning(false);
+}
+
+OscProbCalcerNuFASTEarth::~OscProbCalcerNuFASTEarth() {
+  if (EarthDensity) {delete EarthDensity;}
+}
+
+void OscProbCalcerNuFASTEarth::SetupPropagator() {
+  //=============================
+  PREM_Four* PREMFour = new PREM_Four();
+  EarthDensity = dynamic_cast<Earth_Density*>(PREMFour);
+  //=============================
+
+  ProbEngine.Set_Earth(DetectorDepth, EarthDensity);
+}
+
+void OscProbCalcerNuFASTEarth::CalculateProbabilities(const std::vector<FLOAT_T>& OscParams) {
+ 
+  const double s12sq = OscParams[kTH12];
+  const double s13sq = OscParams[kTH13];
+  const double s23sq = OscParams[kTH23];
+  const double delta = OscParams[kDCP];
+  const double Dmsq21 = OscParams[kDM12];
+
+  //Need to convert OscParams[kDM23] to kDM31
+  const double Dmsq31 = OscParams[kDM23]+OscParams[kDM12]; // eV^2
+
+  for (int iNuType=0;iNuType<fNNeutrinoTypes;iNuType++) {
+
+    bool NeutrinoType = (fNeutrinoTypes[iNuType] == Nu) ? true : false;
+    ProbEngine.Set_Oscillation_Parameters(s12sq, s13sq, s23sq, delta, Dmsq21, Dmsq31, NeutrinoType);
+    ProbEngine.Set_Spectra(fEnergyArray, fCosineZArray);
+    std::vector<std::vector<Matrix3r>> probabilities = ProbEngine.Get_Probabilities();
+    
+    for (int iCosineZPoint=0;iCosineZPoint<fNCosineZPoints;iCosineZPoint++) {
+      for (int iEnergyPoint=0;iEnergyPoint<fNEnergyPoints;iEnergyPoint++) {
+	for (int iOscChannel = 0; iOscChannel < fNOscillationChannels; iOscChannel++) {
+
+	  int Index = ReturnWeightArrayIndex(iNuType,iOscChannel,iEnergyPoint,iCosineZPoint);
+          const int gflv = fOscillationChannels[iOscChannel].GeneratedFlavour-1;
+          const int dflv = fOscillationChannels[iOscChannel].DetectedFlavour-1;
+	  
+	  fWeightArray[Index] = probabilities[iEnergyPoint][iCosineZPoint].arr[gflv][dflv];
+	}
+      } 
+    }
+    
+  }
+
+}
+
+int OscProbCalcerNuFASTEarth::ReturnWeightArrayIndex(int NuTypeIndex, int OscChanIndex, int EnergyIndex, int CosineZIndex) {
+  int IndexToReturn = NuTypeIndex*fNOscillationChannels*fNCosineZPoints*fNEnergyPoints + OscChanIndex*fNCosineZPoints*fNEnergyPoints + EnergyIndex*fNCosineZPoints + CosineZIndex;
+  return IndexToReturn;
+}
+
+long OscProbCalcerNuFASTEarth::DefineWeightArraySize() {
+  long nCalculationPoints = static_cast<long>(fNEnergyPoints) * fNCosineZPoints * fNOscillationChannels * fNNeutrinoTypes;
+  return nCalculationPoints;
+}
